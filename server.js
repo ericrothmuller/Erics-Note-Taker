3cfe8a58811f1157a2938edbb8348727e1ae0015
ericrothmuller <eric@skymarketingco.com>
2022-07-24 22:33:54 -0700

    Added util, and middleware

--- server.js
+++ server.js
@@ -4,23 +4,41 @@ var express = require("express");
 
 var app = express();
 
-var fs = require("fs");
+const fs = require("fs");
+
+const path = require("path");
+
+const util = require("util");
 
 const PORT = 3000;
 
 // Middleware
 
-
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 // Routes
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"))
+app.get("/notes", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/notes.html"))
 });
 
-app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/notes.html"))
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "/public/index.html"))
 });
+
+app.get ("/api/notes", (req, res) => {
+
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+
+})
+
+app.post ("/api/notes", (req, res) => {
+
+})
+
+// Read and Write Notes
+
 
 
 
