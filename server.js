048dc7ae4e31b0d8eea9f27c5bb8bc3d537b9a0c
ericrothmuller <eric@skymarketingco.com>
2022-07-25 02:32:00 -0700

    Restructured the file system

--- server.js
+++ server.js
@@ -10,37 +10,58 @@ const path = require("path");
 
 const util = require("util");
 
+const readFile = util.promisify(fs.readFile);
+const writeFile = util.promisify(fs.writeFile);
+
 const PORT = 3000;
 
 // Middleware
 
+app.use(express.static("/develop/public/assets/css/styles.css"))
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // Routes
 
 app.get("/notes", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/notes.html"))
+    res.sendFile(path.join(__dirname, "/develop/public/index.html"))
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/develop/public/index.html"))
 });
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"))
+    res.sendFile(path.join(__dirname, "/develop/public/index.html"))
 });
 
-app.get ("/api/notes", (req, res) => {
 
-    readFileAsync("/db/db.json", "json")
-    .then( function(notes) {
+app.get("/api/notes", (req, res) => {
 
+    readFile("/develop/db/db.json", "utf8")
+    .then( function(notesdata) {
+        let notelist = [].concat(JSON.parse(notesdata));
+
+        res.json(notelist)
     }
 
 )})
 
 app.post ("/api/notes", (req, res) => {
-
+    const noteBody = req.body;
+    readFile("/develop/db/db.json", "utf8")
+    .then( function(notesdata) {
+        let notelist = [].concat(JSON.parse(notesdata));
+        noteBody.id = notelist.length + 1;
+        notelist.push(notesdata);
+        return notelist;
+    } )
+    .then( function(notelist) {
+        writeFile("/develop/db/db.json", JSON.stringify(notelist))
+        res.json(notesdata);
+    })
 })
 
-// Read and Write Notes
 
 
 
