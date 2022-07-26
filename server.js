a5555add0b2cfe3917ddcd82a0bbc2c7c9a17470
ericrothmuller <eric@skymarketingco.com>
2022-07-25 21:23:16 -0700

    Added another middleware body parser

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const PORT = process.env.PORT || 3001;
 // Middleware
 
 app.use(express.static("./develop/public"));
-// app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
 // Routes
